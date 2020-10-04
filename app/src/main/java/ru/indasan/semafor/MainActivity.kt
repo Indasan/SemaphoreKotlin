89f5ec7358dceab5b48a00de3d607b490ee07925
Indasan <skif_nwn4@mail.ru>
2020-10-05 00:19:38 +0300

    Stable version 1.0

--- app/src/main/java/ru/indasan/semafor/MainActivity.kt
+++ app/src/main/java/ru/indasan/semafor/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import java.util.*
-
+// Ver 1.0
 class MainActivity : Activity() {
     var count:Int = 0;
     var imageArray:IntArray = intArrayOf(R.drawable.semafor_green,R.drawable.semafor_yellow,R.drawable.semafor_red)
